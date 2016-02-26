fef944dc8024d3de7cff14d26e48626e04ad89cb
rizwanhkm <rizwan.hkm@gmail.com>
2016-02-26 12:02:43 -0800

    Remove Unused Code

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,12 @@
 $("#synergy-slider").hide();
 
-function hideslider() {
-    $("document").on("click", function () {
-        $("#synergy-slider").hide(200);
-        $("document").off();
-    });
-}
 $("#nav-bar-button").click(function () {
     $("#synergy-slider").toggle(200);
     setTimeout(function () {
         $("body").on("click", function (e) {
             if (e.pageX > 300) {
                 $("#synergy-slider").hide(200);
-                $("body").off();
+                $("body").off();   
             }
         });
     }, 200);
