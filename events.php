6496344309172dbc2c7de03f7c51d9b697a54d11
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-10 01:25:36 -0800

    Add Maximum No. of Members For Each Event

--- events.php
+++ events.php
@@ -11,8 +11,21 @@ $events = array(
   'paperplane' => "Paper Plane",
   'selfpropellingvehicle' => "Self Propelling Vehicle" ,
   'cadmodelling' => "CAD Modelling",
-  'mcquiz' => "McQuiz",
+  'mcquiz' => "McQuiz"
  );
 
+$eventmembers = array(
+  'fixemup' =>1 ,
+  'engineerofthefuture' => 2,
+  'techyhunt' =>3 ,
+  'junkyardwars' =>2 ,
+  'paperpresentation' =>1,
+  'waterrocketry' => 4,
+  'sanrachana' => 3,
+  'paperplane' => 1,
+  'selfpropellingvehicle' => 3 ,
+  'cadmodelling' => 1,
+  'mcquiz' => 2
+);
 
 ?>
