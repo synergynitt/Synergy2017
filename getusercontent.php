fef5ada1d12b26005b8e9490882f6b20de59c1ba
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-09 23:24:40 -0800

    Change getusercontent.php To return Registered Events

--- getusercontent.php
+++ getusercontent.php
@@ -1,31 +1,54 @@
 <?php
 require 'connect.php';
 require 'events.php';
+require 'functions.php';
+
 session_start();
 
-if (isset($_SESSION['userid'])){
+if (isset($_SESSION['email'])){
 
-  $userid=$_SESSION['userid'];
-
-  $sql = "SELECT * FROM `events` WHERE `userid`=\"$userid\"";
-  if (!$result = $db->query($sql)){
-    $message = array ("status"=>"fail","description"=>"Can't Access Database", "error"=>$db->error);
-    echo json_encode($message);
+  $email=$_SESSION['email'];
+  $url ="http://$_SERVER[HTTP_HOST]/getuserdetails.php?email=$email";
+  $ch=curl_init();
+  curl_setopt_array(
+    $ch, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true
+    )
+  );
+  $response=curl_exec($ch);
+  $response=json_decode($response);
+  if ($response->status == "error"){
+    echo json_encode($response);
     die();
   }
+  $noofgroups= $response->user->noofgroups;
+  $groups= $response->user->groups;
 
-  while ($row=$result->fetch_assoc()) {
-    $registeredEvents = array();
-    $registeredEventsCode = array();
-    foreach ($events as $key => $value) {
-      if ($row[$key]==1){
-        array_push($registeredEvents,"$value");
-        array_push($registeredEventsCode,"$key");
+  $registeredEvents = array();
+  $registeredEventsCode = array();
+  $registeredEventGroupName = array();
+  $registeredEventGroupId = array();
+
+  while($noofgroups){
+    $groupid = $groups[$noofgroups-1]->id;
+    $groupname = $groups[$noofgroups-1]->name;
+    $sql = "SELECT * FROM `events` WHERE `groupid`=\"$groupid\"";
+    $result = executeQuery($db, $sql);
+    while ($row=$result->fetch_assoc()) {
+      foreach ($events as $key => $value) {
+        if ($row[$key]==1){
+          array_push($registeredEvents,"$value");
+          array_push($registeredEventsCode,"$key");
+          array_push($registeredEventGroupName,"$groupname");
+          array_push($registeredEventGroupId,"$groupid");
+        }
       }
     }
-    $message = array ("status"=>"success","registeredEvents"=>$registeredEvents,"registeredEventsCode"=>$registeredEventsCode);
-    echo json_encode($message);
+    $noofgroups--;
   }
+  $message = array ("status"=>"success", "registeredEvents"=>$registeredEvents, "registeredEventsCode"=>$registeredEventsCode, "registeredEventGroupId"=>$registeredEventGroupId, "registeredEventGroupName"=>$registeredEventGroupName);
+  echo json_encode($message);
 
 }else{
   $message = array ("status"=>"logout", "description"=>"Not logged in");
