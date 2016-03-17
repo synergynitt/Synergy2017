7f4996ce6266f048331acadf8f6ff3cf6cc77a15
rizwanhkm <rizwan.hkm@gmail.com>
2016-03-17 11:40:26 +0530

    Fix Bug: Menu Not Showing After Clicking on Menu Button To Close It

--- js/slider.js
+++ js/slider.js
@@ -4,9 +4,12 @@ $("#nav-bar-button").click(function () {
     $("#synergy-slider").toggle(200);
     setTimeout(function () {
         $("body").on("click", function (e) {
+            if(e.target.id == "nav-bar-button"){
+              return;
+            }
             if (e.pageX > 300) {
                 $("#synergy-slider").hide(200);
-                $("body").off();   
+                $("body").off();
             }
         });
     }, 200);
